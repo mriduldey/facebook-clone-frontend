0c155ebba472e2c69468ddb62e10e4dfca4ff76b
Mridul Dey <mridul.dey@aryaka.com>
2022-10-26 19:28:11 +0530

    redux initial setup completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/login/index';
+import Profile from './pages/profile/index';
+import Home from './pages/home/index';
+
 function App() {
-  return <div>welcome to frontend</div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} exact />
+      <Route path="/profile" element={<Profile />} exact />
+      <Route path="/" element={<Home />} exact />
+    </Routes>
+  );
 }
 
 export default App;
